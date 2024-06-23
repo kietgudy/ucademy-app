8c6247720e5df82cb51526c753835c0316815dc9
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-06-23 19:43:45 +0700

    fix layout

---
+++ src/app/(dashboard)/[course]/lesson/page.tsx
@@ -0,0 +1,10 @@
+
+const page = ({params, searchParams} : {params: any; searchParams: any}) => {
+    return (
+        <div>
+          Lesson  Course
+        </div>
+    );
+};
+
+export default page;
---
+++ src/app/study/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>study</div>;
+};
+
+export default page;
