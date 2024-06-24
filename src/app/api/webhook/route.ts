495f19e96100348c78620399d9c064e72018fe1a
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-06-24 23:29:56 +0700

    update webhook

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,4 +1,6 @@
+import createUser from "@/lib/actions/user.actions";
 import { WebhookEvent } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 import { Webhook } from "svix";
 
 export async function POST(req: Request) {
@@ -7,6 +9,9 @@ export async function POST(req: Request) {
   const svix_signature = req.headers.get("svix-signature") ?? "";
   if (!process.env.WEBHOOK_SECRET) {
     throw new Error("WEBHOOK_SECRET is not set");
+  }
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response("Bad Request", { status: 400});
   }
   const payload = await req.json();
   const body = JSON.stringify(payload);
@@ -25,10 +30,21 @@ export async function POST(req: Request) {
     return new Response("Bad Request", { status: 400 });
   }
 
-const eventType = msg.type;
-if(eventType === "user.created") {
-    console.log(msg.data)
-}
+  const eventType = msg.type;
+  if (eventType === "user.created") {
+    const { id, username, email_addresses, image_url } = msg.data;
+    const user = await createUser({
+      username: username!,
+      name: username!,
+      clerkId: id,
+      email: email_addresses[0].email_address,
+      avatar: image_url,
+    });
+    return NextResponse.json({
+      message: "OK",
+      user,
+    });
+  }
 
   // Rest
 
