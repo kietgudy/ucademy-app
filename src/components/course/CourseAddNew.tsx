a38ba5be9eedca76aede25d137db280a4312fec8
kietgudy <nguyenvutankiet17012002@gmail.com>
2024-06-25 16:47:35 +0700

    using slugify & customize input

--- src/components/course/CourseAddNew.tsx
+++ src/components/course/CourseAddNew.tsx
@@ -3,6 +3,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import slugify from "slugify";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -15,6 +16,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { useState } from "react";
 
 const formSchema = z.object({
   title: z.string().min(10, "Tên khóa học phải có ít nhất 10 ký tự"),
@@ -22,6 +24,7 @@ const formSchema = z.object({
 });
 
 function CourseAddNew() {
+  const [isSubmitting, setIsSubmitting] = useState(false);
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -32,42 +35,62 @@ function CourseAddNew() {
   });
 
   function onSubmit(values: z.infer<typeof formSchema>) {
-
-    console.log(values);
+    setIsSubmitting(true);
+    try {
+      const data = {
+        title: values.title,
+        slug:
+          values.slug ||
+          slugify(values.title, {
+            lower: true,
+            locale: "vi",
+          }),
+      };
+    } catch (error) {
+    } finally {
+      setIsSubmitting(false);
+    }
   }
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} autoComplete="off" >
+      <form onSubmit={form.handleSubmit(onSubmit)} autoComplete="off">
         <div className="grid grid-cols-2 gap-8 mt-8 mb-8">
-        <FormField
-          control={form.control}
-          name="title"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Tên khóa học</FormLabel>
-              <FormControl>
-                <Input placeholder="Nhập tên khóa học" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="slug"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Đường dẫn URL</FormLabel>
-              <FormControl>
-                <Input placeholder="khoa-hoc-lap-trinh" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Tên khóa học</FormLabel>
+                <FormControl>
+                  <Input placeholder="Nhập tên khóa học" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="slug"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Đường dẫn URL</FormLabel>
+                <FormControl>
+                  <Input placeholder="khoa-hoc-lap-trinh" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
         </div>
-        <Button type="submit">Tạo khóa học</Button>
+        <Button
+          isLoading={isSubmitting}
+          type="submit"
+          className="w-[120px]"
+          disabled={isSubmitting}
+        >
+          Tạo khóa học
+        </Button>
       </form>
     </Form>
   );
